93bd7ad9cf4ea0ad4ccc47542b6abea5bc2da867
Icaro Pereira De Paula <icaropereiradepaula@gmail.com>
2025-12-24 16:46:59 -0400

    Updates

--- assets/lucide-DXK_mMCC.js
+++ assets/lucide-DXK_mMCC.js
@@ -1,4 +1,4 @@
-import{r as y}from"./framer-motion-CV1iBef-.js";/**
+import{r as y}from"./framer-motion-Df2DHHj8.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
---
+++ assets/vendor-charts-CCyocjbH.js
@@ -0,0 +1,1 @@
+import"./framer-motion-Df2DHHj8.js";import"./radix-ui-lLXGAJAj.js";
